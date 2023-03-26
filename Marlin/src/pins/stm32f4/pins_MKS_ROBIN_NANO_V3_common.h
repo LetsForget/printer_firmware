3306e42cb2c40daa132ff02dfc7595ba50c26b20
Mikhail Krylov <mihail.krylov@playgendary.com>
2023-03-26 22:24:15 +0600

    Убрал пин с нагревателя, потому что он на вентиляторе

--- Marlin/src/pins/stm32f4/pins_MKS_ROBIN_NANO_V3_common.h
+++ Marlin/src/pins/stm32f4/pins_MKS_ROBIN_NANO_V3_common.h
@@ -136,7 +136,7 @@
 // Heaters / Fans
 //
 #define HEATER_0_PIN                        PE5   // HEATER1
-#define HEATER_1_PIN                        PB0   // HEATER2
+//#define HEATER_1_PIN                        PB0   // HEATER2
 #define HEATER_BED_PIN                      PA0   // HOT BED
 
 #define FAN_PIN                             PC14  // FAN
